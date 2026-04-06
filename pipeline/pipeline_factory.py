eb6503633b12451de3ad80e8491fc6fd3dbf7fc2
Lucas Fraga <ti.lucasfraga@gmail.com>
2026-04-06 14:54:08 -0300

    refactor: Remove ClaimDecomposer and CounterfactualGenerator from pipeline functions

--- pipeline/pipeline_factory.py
+++ pipeline/pipeline_factory.py
@@ -1,7 +1,5 @@
 from config import Config
 
-from modules.claim_decomposition.claim_decomposer import ClaimDecomposer
-from modules.counterfactual.counterfactual_generator import CounterfactualGenerator
 from modules.question_generation.question_batch_generator import QuestionBatchGenerator
 from modules.question_generation.question_iterative_generator import QuestionIterativeGenerator
 from modules.search.search_factory import get_searcher
@@ -56,10 +54,7 @@ def averitec_original_pipeline():
 
 
 
-def averitec_iterative_pipeline(
-    use_atomic=False,
-    use_counterfactual=False,
-):
+def averitec_iterative_pipeline():
     llm = OllamaLLM(Config.OLLAMA_MODEL)
 
     pipeline = IterativePipeline(
@@ -87,92 +82,6 @@ def averitec_iterative_pipeline(
         verdict_predictor=LLMVerdict(llm),
 
         justification_generator=JustificationGenerator(llm),
-
-        # 🆕 plugáveis
-        claim_decomposer=ClaimDecomposer(llm) if use_atomic else None,
-        counterfactual_module=CounterfactualGenerator(llm) if use_counterfactual else None,
     )
 
     return pipeline
-
-
-
-
-
-def averitec_original_counterfactual_pipeline():
-    
-    llm = OllamaLLM(Config.OLLAMA_MODEL)
-
-    pipeline = BatchPipeline(
-        question_generator=QuestionBatchGenerator(llm),
-        searcher=get_searcher(),
-        parser=DocumentParser(),
-
-        retriever=BM25Retriever(
-            top_k=Config.BM25_TOP_K
-        ),
-
-        segmenter=PassageExtractor(
-            chunk_size=Config.CHUNK_SIZE
-        ),
-
-        reranker=CrossEncoderReranker(
-            model_name=Config.RERANKER_MODEL,
-            top_k=Config.RERANKER_TOP_K,
-            threshold=Config.RERANKER_THRESHOLD
-        ),
-
-        qa_generator=QAGenerator(llm),
-        stance_detector=LLMStanceDetector(llm),
-
-        verdict_predictor=LLMVerdict(llm),
-        justification_generator=JustificationGenerator(llm),
-
-        counterfactual_module=CounterfactualGenerator(llm)
-    )
-
-    return pipeline
-
-
-
-
-
-def averitec_iterative_counterfactual_pipeline(
-    use_atomic=False,
-    use_counterfactual=False,
-):
-    llm = OllamaLLM(Config.OLLAMA_MODEL)
-
-    pipeline = IterativePipeline(
-        question_generator=QuestionIterativeGenerator(llm),
-        searcher=get_searcher(),
-        parser=DocumentParser(),
-
-        retriever=BM25Retriever(
-            top_k=Config.BM25_TOP_K
-        ),
-
-        segmenter=PassageExtractor(
-            chunk_size=Config.CHUNK_SIZE
-        ),
-
-        reranker=CrossEncoderReranker(
-            model_name=Config.RERANKER_MODEL,
-            top_k=Config.RERANKER_TOP_K,
-            threshold=Config.RERANKER_THRESHOLD
-        ),
-
-        qa_generator=QAGenerator(llm),
-        stance_detector=LLMStanceDetector(llm),
-
-        verdict_predictor=LLMVerdict(llm),
-
-        justification_generator=JustificationGenerator(llm),
-
-        # 🆕 plugáveis
-        claim_decomposer=ClaimDecomposer(llm) if use_atomic else None,
-        counterfactual_module=CounterfactualGenerator(llm) if use_counterfactual else None,
-    )
-
-    return pipeline
-
